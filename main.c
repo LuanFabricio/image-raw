2e3328c2aab47453698b3b161790d2cc58f35b6c
LuanFabricio <luanlimaleite@hotmail.com>
2025-03-18 23:41:53 -0300

    feat: implements -save-image for folders

--- main.c
+++ main.c
@@ -1,39 +1,115 @@
+#include <stdlib.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <dirent.h>
 
 #include "raylib.h"
 #include "raymath.h"
 
 #define RAYLIB_TITLE "IDK"
-#define IMAGE_SIZE 96
+#define IMAGE_SIZE 112
 
-void save_image(const char* input_file, const char* output_file)
+typedef enum {
+	GRAYSCALE = 0,
+	RGB,
+} ImageColorEnum;
+
+int list_dir(char* folder, char** buffer, int *len)
+{
+	DIR *dir;
+	struct dirent *ep;
+
+	dir = opendir(folder);
+
+	if (dir == NULL) {
+		return 0;
+	}
+
+	*len = 0;
+	while((ep = readdir(dir)) != NULL) {
+		bool is_invalid = strcmp(ep->d_name, ".") == 0 || strcmp(ep->d_name, "..") == 0;
+		if (is_invalid) continue;
+
+		printf("[%i]: %s(%u/%lu)\n", *len, ep->d_name, ep->d_reclen, strlen(ep->d_name));
+		(*len)++;
+	}
+
+	printf("Len: %i(%lu)\n", *len, sizeof(char**) * (*len));
+	seekdir(dir, 0);
+	int index = 0;
+	while((ep = readdir(dir)) != NULL) {
+		bool is_invalid = strcmp(ep->d_name, ".") == 0 || strcmp(ep->d_name, "..") == 0;
+		if (is_invalid) continue;
+
+		buffer[index] = malloc(sizeof(char*) * strlen(ep->d_name));
+		strncpy(buffer[index], ep->d_name, strlen(ep->d_name));
+		printf("%s\n", buffer[index]);
+		index++;
+	}
+
+	closedir(dir);
+	return 1;
+}
+
+void save_image_grayscale(uint8_t* img_bytes, const size_t img_size, const Color* og_colors)
+{
+	for (int i = 0; i < img_size; i++) {
+		uint8_t gray = (og_colors[i].r + og_colors[i].g + og_colors[i].b) / 3;
+		img_bytes[i] = gray;
+	}
+}
+
+void save_image_rgb(uint8_t* img_bytes, const size_t img_size, const uint8_t* og_img_colors)
+{
+	int img_idx = 0;
+	for (int i = 0; i < img_size*4; i++) {
+		img_bytes[img_idx++] = og_img_colors[i++];
+		img_bytes[img_idx++] = og_img_colors[i++];
+		img_bytes[img_idx++] = og_img_colors[i++];
+	}
+}
+
+void save_image(const char* input_file, const char* output_file, const int img_format)
 {
 	Image img = LoadImage(input_file);
 
-	printf("Image width: %d\n", img.width);
-	printf("Image height: %d\n", img.height);
+	printf("[Old] Image width: %d\n", img.width);
+	printf("[Old] Image height: %d\n", img.height);
 
 	ImageResize(&img, IMAGE_SIZE, IMAGE_SIZE);
 	ImageFormat(&img, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
 
-	Color *data = (Color*)img.data;
-	uint8_t img_bytes[IMAGE_SIZE*IMAGE_SIZE];
+	printf("[New] Image width: %d\n", img.width);
+	printf("[New] Image height: %d\n", img.height);
 
-	for (int i = 0; i < IMAGE_SIZE*IMAGE_SIZE; i++) {
-		uint8_t gray = (data[i].r + data[i].g + data[i].b) / 3;
-		img_bytes[i] = gray;
-	}
+	size_t img_channels = 1;
+	if (img_format == RGB) img_channels = 3;
+	const size_t img_size = IMAGE_SIZE * IMAGE_SIZE;
+
+	const size_t img_bytes_len = img_size * img_channels;
+	uint8_t img_bytes[IMAGE_SIZE*IMAGE_SIZE*3] = {0};
+
+	if (img_format == RGB) save_image_rgb(img_bytes, img_size, img.data);
+	else save_image_grayscale(img_bytes, img_size, img.data);
+
 
 	FILE* file = fopen(output_file, "wb");
-	fwrite(img_bytes, 1, IMAGE_SIZE*IMAGE_SIZE, file);
+	fwrite(img_bytes, 1, img_bytes_len, file);
 	fclose(file);
 }
 
-void view_image(const char* filename)
+void view_image(const char* filename, int img_format)
 {
-	Image img = LoadImageRaw(filename, IMAGE_SIZE, IMAGE_SIZE, PIXELFORMAT_UNCOMPRESSED_GRAYSCALE, 0);
+	int format = PIXELFORMAT_UNCOMPRESSED_GRAYSCALE;
+	if (img_format == RGB) format = PIXELFORMAT_UNCOMPRESSED_R8G8B8;
+
+	Image img = LoadImageRaw(filename, IMAGE_SIZE, IMAGE_SIZE, format, 0);
+	// Image img = LoadImage(filename);
+	// ImageResize(&img, IMAGE_SIZE, IMAGE_SIZE);
+	// ImageFormat(&img, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
+
 	printf("Image width: %d\n", img.width);
 	printf("Image height: %d\n", img.height);
 	printf("Image format: %d\n", img.format);
@@ -95,12 +171,40 @@ void view_image(const char* filename)
 
 int main(int argc, char* argv[])
 {
-	if (argc > 2 && strcmp(argv[1], "-load-image") == 0) {
-		save_image(argv[2], argv[3]);
+	char *folder_buffer[1024] = {};
+	int len;
+	int img_format = GRAYSCALE;
+
+	if (argc > 2 && strcmp(argv[1], "-save-image") == 0) {
+		for (int i = 0 ; i < argc; i++) printf("argv[%i]: %s\n", i, argv[i]);
+		if (argc > 4 && strcmp(argv[5], "rgb") == 0) img_format = RGB;
+
+		if (argc > 2 && strcmp(argv[2], "folder") == 0) {
+			if (!list_dir(argv[3], folder_buffer, &len)) printf("To read folder '%s'\n", argv[5]);
+
+			for (int i = 0; i < len; i++) {
+				char buffer_folder_input[128] = "";
+				strcpy(buffer_folder_input, argv[3]);
+				char buffer_folder_output[128] = "";
+				strcpy(buffer_folder_output, argv[4]);
+
+				char buffer_in[1024] = "";
+				strcat(strcat(buffer_in, buffer_folder_input), folder_buffer[i]);
+				char buffer_out[1024] = "";
+				strcat(strcat(buffer_out, strcat(buffer_folder_output, folder_buffer[i])), ".raw");
+
+				printf("Buffer in: %s\n", buffer_in);
+				printf("Buffer out: %s\n", buffer_out);
+				save_image(buffer_in, buffer_out, img_format);
+			}
+		} else {
+			save_image(argv[3], argv[4], img_format);
+		}
 	}
 
 	if (argc > 2 && strcmp(argv[1], "-view-image") == 0) {
-		view_image(argv[2]);
+		if (argc > 3 && strcmp(argv[3], "rgb") == 0) img_format = RGB;
+		view_image(argv[2], img_format);
 	}
 
 	return 0;
